754601bf2d4d0aeda38470d96c9bd5ec6ff89aef
Jeff Richards <jeff.richards@tradingtechnologies.com>
2013-09-30 16:53:03 -0500

    In work

--- prog.asm
+++ prog.asm
@@ -1,20 +1,39 @@
 ;Initial creation of variables
-equ inc_value 2
-equ answer 1
+var inc_value 2
+var answer 1
+var count 100
 
-load answer
 ; Main loop
 :loop
-call func
-testim 100
+push answer
+testim count
+pop
 contlt done
+storebp
+call func
 jump loop
 
 ; The multiply function
 :func
-load inc_value
+push answer
+push inc_value
 mult
-return ; Leave answer on stack
+store answer
+return
 
 :done
+store answer
+call printtest
 halt
+
+:printtest
+storebp
+push "Hello"
+push "World"
+push 1
+push pop
+call  Print
+break
+return
+
+
--- vmtest.cpp
+++ vmtest.cpp
@@ -5,16 +5,65 @@
 #include "../VMEngine/machine.h"
 #include "../VMEngine/Assembler.h"
 #include "../VMEngine/Data.h"
+#include "../VMEngine/Function.h"
 #include <fstream>
 
 using namespace vm;
+using namespace std;
+
+class Print : public Function
+{
+    void printit(Machine& machine, Data *pData)
+    {
+        switch (pData->type)
+        {
+            case Data::INT:
+                {
+                    Int *p = (Int *)pData;
+                    cout << "Print: " << p->n << endl;
+                }
+                break;
+            case Data::STRING:
+                {
+                    String *p = (String *)pData;
+                    cout << "Print: " << p->str << endl;
+                }
+                break;
+            case Data::VARIABLE:
+                {
+                    Variable *var = (Variable *)pData;
+                    Data *pData;
+                    if (machine.GetVariable(var->name, pData))
+                    {
+                        printit(machine, pData);
+                    }
+                    else
+                    {
+                        cout << "Variable " << var->name << " not found" << endl;
+                    }
+                }
+                break;
+        }
+    }
+
+    void OnExecute(Machine& machine)
+    {
+        Data *pData = machine.stack.Pop();
+        printit(machine, pData);
+    }
+};
 
 int _tmain(int argc, _TCHAR* argv[])
 {
     Machine machine;
     Assembler assem;
     std::ifstream strm;
-    strm.open("prog.a");
+    strm.open("prog.asm");
+    if (strm.is_open() == false)
+    {
+        std::cout <<  "Cannot open file" << std::endl;
+        return 1;
+    }
     try
 	{
         assem.Assemble(machine, strm);
@@ -25,11 +74,13 @@ int _tmain(int argc, _TCHAR* argv[])
         return 1;
 	}
 
+    Print print;
+    machine.RegisterFunction("Print", &print);
     while (machine.IsHalted() == false)
     {
         try
 		{
-            machine.DumpMachine(std::cout);
+            //machine.DumpMachine(std::cout);
             machine.Clock();
 		}
         catch (std::exception& ex)
