07bcf581e0b1c3021b3222591f5cdea8a6a0db0a
tt-jsr <jeff.richards@tradingtechnologies.com>
2013-10-05 11:26:44 -0500

    Added split test

--- prog.asm
+++ prog.asm
@@ -1,21 +1,39 @@
-var instname ""
-
 call testStrCmp
 call testSubStr
 call testStrCat
 call testSwap
 call testDup
+call testSplit
 halt
 
 
 ;---------------------------------------
-;function testSplit
-;push "one,two,three,four"
-;split ","
-;push 0
-;elem_get 
-;calllib  print
-;return
+function testSplit
+push "one,two,three,four,five"
+push ","
+calllib strsplit    ; array
+dup                 ; array array
+store array         ; array
+calllib arr_size    ; size
+store size          ; <empty>
+push 0              ; 0
+store count         ; <empty>
+:again
+load array          ; array
+load count          ; array count
+calllib arr_get     ; string
+calllib println     ; <empty>
+load count          ; count
+inc                 ; count
+dup                 ; count count
+store count         ; count
+load size           ; count size
+swap                ; size count
+test                ; size count with flag set
+pop                 ; size
+pop                 ; <empty>
+jumplt again
+return
 ;---------------------------------------
 ; Test dup
 function testDup
